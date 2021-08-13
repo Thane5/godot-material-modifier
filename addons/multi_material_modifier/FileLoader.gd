57f1e6588aed2cfa124af5b9ec66008d9c6a1ef0
Thane5 <13683581+Thane5@users.noreply.github.com>
2021-08-13 21:51:09 +0200

    Minor cleanups

--- addons/multi_material_modifier/FileLoader.gd
+++ addons/multi_material_modifier/FileLoader.gd
@@ -18,14 +18,14 @@ func _on_FileDialog_Mat_files_selected(paths):
 
 	if paths.size() == 1:
 		get_node("LoadPath").set_text(paths[0])
-#		multiMode = false
+
 
 	else:
 		var lastSlash = paths[0].rfind("/")
 		var text = paths[0].rstrip(paths[0].substr(lastSlash+1))
 
 		get_node("LoadPath").set_text(text + " [Multiple]")
-#		multiMode = true
+
 
 
 #	In cas the user wants to manually paste in a path
