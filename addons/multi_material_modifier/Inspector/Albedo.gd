12517eadcef9b70148b1dcb4f50a9e12b686b605
Thane5 <13683581+Thane5@users.noreply.github.com>
2021-08-13 12:59:31 +0200

    Fixed Editor Theme Compatibility

--- addons/multi_material_modifier/Inspector/Albedo.gd
+++ addons/multi_material_modifier/Inspector/Albedo.gd
@@ -4,16 +4,19 @@ extends PanelContainer
 var albedoColor 
 var active = false
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 
+func _enter_tree():
+	var style1 = load("res://addons/multi_material_modifier/Inspector/ExpandArea_header.tres")
+	style1.set_default_margin(MARGIN_TOP, -1)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	albedoColor = get_node("VBoxContainer/Albedo Value/ColorPickerButton").get_pick_color()
-
+	
 
 
 func _on_Albedo_CheckBox_toggled(button_pressed):
