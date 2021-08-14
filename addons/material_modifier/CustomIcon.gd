50b997ee569b60659f7b6d78a7ed55d9a8836047
Thane5 <13683581+Thane5@users.noreply.github.com>
2021-08-14 15:13:27 +0200

    Renamed addon folder

--- addons/material_modifier/CustomIcon.gd
+++ addons/material_modifier/CustomIcon.gd
@@ -5,7 +5,7 @@ export(String) var customIcon
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _enter_tree():
 	icon = get_icon(customIcon, 'EditorIcons')
 	set_button_icon(icon)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
