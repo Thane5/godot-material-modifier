6e9546b7e1b36aacdb5888f26665b087c01a30fe
Thane5 <clohrffb@gmail.com>
2021-08-15 13:33:54 +0200

    Updated path in init script to fix addon not loading

--- addons/material_modifier/Addon_init.gd
+++ addons/material_modifier/Addon_init.gd
@@ -4,7 +4,7 @@ extends EditorPlugin
 var dock
 
 func _enter_tree():
-	dock = preload("res://addons/multi_material_modifier/MaterialModifier_Dock.tscn").instance()
+	dock = preload("res://addons/material_modifier/MaterialModifier_Dock.tscn").instance()
 	add_control_to_dock(DOCK_SLOT_RIGHT_BL, dock)
 	
 
