5fd36a5c13da736bf6ee982030a8b0a657f211e6
Thane5 <13683581+Thane5@users.noreply.github.com>
2022-12-29 10:32:27 +0100

    Update for Godot4 beta10

--- addons/material_modifier/FileLoader.gd
+++ addons/material_modifier/FileLoader.gd
@@ -29,7 +29,7 @@ func _on_FileDialog_Mat_files_selected(paths):
 		var text = paths[0].rstrip(paths[0].substr(lastSlash+1))
 		
 #		get_node("LoadPath").set_text(text + " [Multiple]")
-		get_node("LoadPath").set_text(text + " [" + var2str(paths.size()) + " Selected]")
+		get_node("LoadPath").set_text(text + " [" + var_to_str(paths.size()) + " Selected]")
 
 
 ##	In case the user wants to manually paste in a path
